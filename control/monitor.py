5dc2112b9e4f40d99ce533f195d8b3511cb79c4e
Oscar Buitrago <o.buitragov@uniandes.edu.co>
2024-09-16 20:42:49 -0500

    Agrega nueva consulta para luminosidad y nuevas condiciones

--- control/monitor.py
+++ control/monitor.py
@@ -11,6 +11,68 @@ from django.conf import settings
 client = mqtt.Client(settings.MQTT_USER_PUB)
 
 
+def analyze_lux():
+    # Consulta los datos de los últimos 5 minutos para la medida de luminosidad
+    # Compara el promedio con los valores con los límites que están en la base de datos para esa variable.
+    # Si el promedio se excede de los límites, se envía un mensaje para los actuadores.
+
+    print("Calculando alertas...")
+
+    data = Data.objects.filter(
+        base_time__gte=datetime.now() - timedelta(minutes=5),
+        measurement__name="luminosidad",
+    )
+
+    aggregation = (
+        data.annotate(check_value=Avg("avg_value"))
+        .select_related("station", "measurement")
+        .select_related("station__user", "station__location")
+        .select_related(
+            "station__location__city",
+            "station__location__state",
+            "station__location__country",
+        )
+        .values(
+            "check_value",
+            "station__user__username",
+            "measurement__name",
+            "measurement__max_value",
+            "measurement__min_value",
+            "station__location__city__name",
+            "station__location__state__name",
+            "station__location__country__name",
+        )
+    )
+    alerts = 0
+    for item in aggregation:
+
+        variable = item["measurement__name"]
+        max_value = item["measurement__max_value"] or 0
+        min_value = item["measurement__min_value"] or 0
+
+        country = item["station__location__country__name"]
+        state = item["station__location__state__name"]
+        city = item["station__location__city__name"]
+        user = item["station__user__username"]
+
+        if item["check_value"] > max_value:
+            message = "APAGAR LUCES".format(variable, min_value, max_value)
+            topic = "{}/{}/{}/{}/in".format(country, state, city, user)
+            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            client.publish(topic, message)
+            alerts += 1
+
+        if item["check_value"] < min_value:
+            message = "ENCENDER LUCES".format(variable, min_value, max_value)
+            topic = "{}/{}/{}/{}/in".format(country, state, city, user)
+            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
+            client.publish(topic, message)
+            alerts += 1
+
+    print(len(aggregation), "dispositivos revisados")
+    print(alerts, "alertas enviadas")
+
+
 def analyze_data():
     # Consulta todos los datos de la última hora, los agrupa por estación y variable
     # Compara el promedio con los valores límite que están en la base de datos para esa variable.
@@ -18,20 +80,27 @@ def analyze_data():
 
     print("Calculando alertas...")
 
-    data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Avg('avg_value')) \
-        .select_related('station', 'measurement') \
-        .select_related('station__user', 'station__location') \
-        .select_related('station__location__city', 'station__location__state',
-                        'station__location__country') \
-        .values('check_value', 'station__user__username',
-                'measurement__name',
-                'measurement__max_value',
-                'measurement__min_value',
-                'station__location__city__name',
-                'station__location__state__name',
-                'station__location__country__name')
+    data = Data.objects.filter(base_time__gte=datetime.now() - timedelta(hours=1))
+    aggregation = (
+        data.annotate(check_value=Avg("avg_value"))
+        .select_related("station", "measurement")
+        .select_related("station__user", "station__location")
+        .select_related(
+            "station__location__city",
+            "station__location__state",
+            "station__location__country",
+        )
+        .values(
+            "check_value",
+            "station__user__username",
+            "measurement__name",
+            "measurement__max_value",
+            "measurement__min_value",
+            "station__location__city__name",
+            "station__location__state__name",
+            "station__location__country__name",
+        )
+    )
     alerts = 0
     for item in aggregation:
         alert = False
@@ -40,17 +109,17 @@ def analyze_data():
         max_value = item["measurement__max_value"] or 0
         min_value = item["measurement__min_value"] or 0
 
-        country = item['station__location__country__name']
-        state = item['station__location__state__name']
-        city = item['station__location__city__name']
-        user = item['station__user__username']
+        country = item["station__location__country__name"]
+        state = item["station__location__state__name"]
+        city = item["station__location__city__name"]
+        user = item["station__user__username"]
 
         if item["check_value"] > max_value or item["check_value"] < min_value:
             alert = True
 
         if alert:
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
-            topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
+            topic = "{}/{}/{}/{}/in".format(country, state, city, user)
             print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
             client.publish(topic, message)
             alerts += 1
@@ -60,26 +129,26 @@ def analyze_data():
 
 
 def on_connect(client, userdata, flags, rc):
-    '''
+    """
     Función que se ejecuta cuando se conecta al bróker.
-    '''
+    """
     print("Conectando al broker MQTT...", mqtt.connack_string(rc))
 
 
 def on_disconnect(client: mqtt.Client, userdata, rc):
-    '''
+    """
     Función que se ejecuta cuando se desconecta del broker.
     Intenta reconectar al bróker.
-    '''
+    """
     print("Desconectado con mensaje:" + str(mqtt.connack_string(rc)))
     print("Reconectando...")
     client.reconnect()
 
 
 def setup_mqtt():
-    '''
+    """
     Configura el cliente MQTT para conectarse al broker.
-    '''
+    """
 
     print("Iniciando cliente MQTT...", settings.MQTT_HOST, settings.MQTT_PORT)
     global client
@@ -89,23 +158,25 @@ def setup_mqtt():
         client.on_disconnect = on_disconnect
 
         if settings.MQTT_USE_TLS:
-            client.tls_set(ca_certs=settings.CA_CRT_PATH,
-                           tls_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_NONE)
+            client.tls_set(
+                ca_certs=settings.CA_CRT_PATH,
+                tls_version=ssl.PROTOCOL_TLSv1_2,
+                cert_reqs=ssl.CERT_NONE,
+            )
 
-        client.username_pw_set(settings.MQTT_USER_PUB,
-                               settings.MQTT_PASSWORD_PUB)
+        client.username_pw_set(settings.MQTT_USER_PUB, settings.MQTT_PASSWORD_PUB)
         client.connect(settings.MQTT_HOST, settings.MQTT_PORT)
 
     except Exception as e:
-        print('Ocurrió un error al conectar con el bróker MQTT:', e)
+        print("Ocurrió un error al conectar con el bróker MQTT:", e)
 
 
 def start_cron():
-    '''
+    """
     Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
-    '''
+    """
     print("Iniciando cron...")
-    schedule.every(10).seconds.do(analyze_data)
+    schedule.every(10).seconds.do(analyze_lux)
     print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()
